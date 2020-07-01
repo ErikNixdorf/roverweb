a3145d94a90ce19f12b65fccf7eb520e0d86c280
ErikNixdorf <erik.nixdorf@ufz.de>
2020-07-01 11:28:20 +0200

    fixed new API

--- test_roverweb.py
+++ test_roverweb.py
@@ -8,6 +8,7 @@ A small example of how to use the code of roverweb
 from roverweb import *
 import roverweb as rw
 import geopandas as gpd
+from datetime import datetime
 # load our dataset as geodataframe
 gdfRover = rw.geometry.from_csv(
     './testdata/Mueglitz-20190708_selection.csv',
@@ -21,9 +22,9 @@ gdfRover_circls = rw.geometry.points_to_circle(
 
 # instead we can also create a true footprint which consideres that the rover is moving,
 # basically a parallelogram attached on both sides with two half circles
-gdfRover_polyg=rw.geometry.points_to_footprint(gdfRover,footprint_radius=50,crs_src = "epsg:4326",crs_dst = "epsg:25833",number_of_points=10,inpt_geo_type='Point')
+gdfRover_polyg=rw.geometry.points_to_footprint(gdfRover,footprint_radius=50,crs_src = "epsg:4326",crs_dst = "epsg:4326",number_of_points=10,inpt_geo_type='Point')
 # Next is that we cluster our datasets in order to improve query speed
-clusters = 3  # number of clusters
+clusters = 1  # number of clusters
 gdfRover_clust = rw.geometry.clustering(
     gdfRover_polyg, clusters=clusters, cluster_fld_name='ClusterID')
 # %%having our clustered dataset we can run our osm to get desired secondary
@@ -61,24 +62,40 @@ for cluster_no in range(0, clusters):
     # append subset back to entire dataset
     gdfRover_osm = gdfRover_osm.append(gdfRover_subset,sort=True)
     
-# Finally we get humidity values from dwd data, 
-gdfRover_osm_sg = rw.weather.apnd_dwd_stationdata(
-    gdfRover_osm.copy(),
-     time_col='Date Time(UTC)',
-     data_time_format='%Y-%m-%d %H:%M:%S',
-     dwd_time_format='%Y%m%d%H',
-     data_category='air_temperature',
-     parameters=['2m_air_temperature','2m_relative_humidity'],
-     temp_resolution='hourly',
-     no_of_nearest_stations=4)   
+#%% Finally we get humidity values from dwd data,
+no_of_nearest_stations=4
+#create temporary columns from dwd data extraction
+dwd_temporary_columns=['air_temperature'+'_station_'+str(i) for i in range(0,no_of_nearest_stations)]
+dwd_temporary_columns.extend(['air_temperature'+'_distance_'+str(i) for i in range(0,no_of_nearest_stations)])
+#correct index to start with 0
+gdfRover_osm=gdfRover_osm.reset_index(drop=True)
+#find nearest stations
+gdfRover_osm_dwd_raw,dwd_base=rw.weather.Find_nearest_dwd_stations(gdfRover_osm.copy(),
+    date_start=datetime.strptime(gdfRover_osm.iloc[0]['Date Time(UTC)'],'%Y-%m-%d %H:%M:%S').date().strftime('%Y%m%d'),
+    date_end=datetime.strptime(gdfRover_osm.iloc[-1]['Date Time(UTC)'],'%Y-%m-%d %H:%M:%S').date().strftime('%Y%m%d'),
+    dwd_time_format='%Y%m%d%H',
+    data_category='air_temperature',
+    temp_resolution='hourly',
+    no_of_nearest_stations=no_of_nearest_stations,
+    memory_save=True,
+    Output=True)
+#add data
+gdfRover_osm_dwd=rw.weather.Apnd_dwd_data(gdfRover_osm_dwd_raw,
+                                          dwd_base,
+                                          time_col='Date Time(UTC)',
+                                          data_time_format='%Y-%m-%d %H:%M:%S',
+                                          data_category='air_temperature',
+                                          parameters=['2m_air_temperature','2m_relative_humidity'],
+                                          no_of_nearest_stations=no_of_nearest_stations,
+                                          idw_exponent=2
+                                          )
+#remove unneccesary colums
+gdfRover_osm_dwd.drop(columns=dwd_temporary_columns,inplace=True)
 # %% Next we add data from soilgrid_network from wcs (faster than restapi)
-gdfRover_osm_sg2 = rw.soilgrid.apnd_from_wcs(
-    gdfRover_osm_sg.copy(),
-    soilgridlrs={
-        'sg250m:ORCDRC_M_sl1_250m': 'ORCDRC_sl1',
-        'sg250m:BLDFIE_M_sl1_250m': 'BLDFIE_sl1',
-        'sg250m:CLYPPT_M_sl1_250m': 'CLYPPT_sl1'
-    },
+gdfRover_osm_dwd_sg = rw.soilgrid.apnd_from_wcs(
+    gdfRover_osm_dwd.copy(),
+    soilgridlrs=['sand','silt'],
+    soil_layerdepths=['60-100cm'],
     raster_res=(250, 250),
     statistical_metric=['mean'],
     all_touched=True,
@@ -87,11 +104,9 @@ gdfRover_osm_sg2 = rw.soilgrid.apnd_from_wcs(
 
 
 #finally we write out our results to an shapefile
-gdfRover_osm_sg_2.to_csv('./output/Mueglitz-20190708_'
+gdfRover_osm_dwd_sg.to_csv('./output/Mueglitz-20190708_'
                        'selection_roverweb.csv')
 
-#change datetime to str
-gdfRover_osm_sg_2['Date Time(UTC)']=gdfRover_osm_sg_2['Date Time(UTC)'].dt.strftime('%Y-%m-%d %H:%M:%S%')
 #save as shp
-gdfRover_osm_sg_2.to_file('./output/Mueglitz-20190708_'
+gdfRover_osm_dwd_sg.to_file('./output/Mueglitz-20190708_'
                        'selection_roverweb.shp')
